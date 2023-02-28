5eb8865bb617d58447b0c7eac343a0e3bbd1f636
KochamLeosiee <99250960+KochamLeosiee@users.noreply.github.com>
2023-02-28 13:02:21 +0100

    Add files via upload

--- strona1.php
+++ strona1.php
@@ -25,7 +25,43 @@
         }else{
             echo "błąd połączenia z bazą";
         }
+        mysqli_close($polaczenie);
     ?>
+    <label for="przedmiot">Wybierz przedmiot</label>
+    <select name="przedmiot" id="przedmiot">
+        <?php
+            for($i=0;$i<count($przedmiot);$i++){
+                echo "<option value=$idprzedmiot[$i]> $przedmiot[$i]</option>";
+            }
+        ?>
+    </select>
+
+    <?php 
+        $polaczenie=mysqli_connect('localhost','root','','4ti');
+        if($polaczenie){
+            echo "Połączyliśmy się z bazą";
+            $zapytanie="SELECT * FROM dane";
+            $wynik=mysqli_query($polaczenie,$zapytanie);
+            while($wiersz=mysqli_fetch_array($wynik)){
+                $imie[]=$wiersz['imie'];
+                $nazwisko[]=$wiersz['nazwisko'];
+                $iddane[]=$wiersz['id'];
+
+            }
+        }else{
+            echo "błąd połączenia z bazą";
+        }
+        mysqli_close($polaczenie);
+    ?>
+
+        <label for="dane">Wybierz dane</label>
+    <select name="dane" id="dane">
+        <?php
+            for($i=0;$i<count($imie);$i++){
+                echo "<option value=$iddane[$i],> $imie[$i],$nazwisko[$i]</option>";
+            }
+        ?>
+    </select>
 </main>
     <footer>
         Strone wykonał Aleksander
