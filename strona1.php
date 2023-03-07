ef36e96b0520303f3e1caf41b80d9220b43008b6
KochamLeosiee <99250960+KochamLeosiee@users.noreply.github.com>
2023-03-07 15:17:41 +0100

    Add files via upload

--- strona1.php
+++ strona1.php
@@ -1,70 +1,79 @@
 <!DOCTYPE html>
-<html lang="pl">
+<html lang="pl=PL">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dowawanie ocen</title>
+    <title>Dodawanie ocen</title>
 </head>
 <body>
     <header>
         <h1>Dodawanie ocen</h1>
     </header>
     <main>
-    <?php 
-        $polaczenie=mysqli_connect('localhost','root','','4ti');
-        if($polaczenie){
-            echo "Połączyliśmy się z bazą";
-            $zapytanie="SELECT * FROM przedmiot";
-            $wynik=mysqli_query($polaczenie,$zapytanie);
-            while($wiersz=mysqli_fetch_array($wynik)){
-                $przedmiot[]=$wiersz['nazwaPrzedmiot'];
-                $idprzedmiot[]=$wiersz['id'];
-
-            }
-        }else{
-            echo "błąd połączenia z bazą";
-        }
-        mysqli_close($polaczenie);
-    ?>
-    <label for="przedmiot">Wybierz przedmiot</label>
-    <select name="przedmiot" id="przedmiot">
         <?php
-            for($i=0;$i<count($przedmiot);$i++){
-                echo "<option value=$idprzedmiot[$i]> $przedmiot[$i]</option>";
+            $connect=mysqli_connect('localhost','root','','4ti');
+            if($connect){
+                echo "Połączyliśmy się z bazą";
+                $zapytanie="SELECT * from przedmiot";
+                $wynik=mysqli_query($connect,$zapytanie);
+                while($wiersz=mysqli_fetch_array($wynik)){
+                    $przedmiot[]=$wiersz['nazwaPrzedmiot'];
+                    $idprzedmiot[]=$wiersz['id'];
+                }
+                $zapytanie="SELECT * from dane";
+                $wynik=mysqli_query($connect,$zapytanie);
+                while($wiersz=mysqli_fetch_array($wynik)){
+                    $imie[]=$wiersz['imie'];
+                    $nazwisko[]=$wiersz['nazwisko'];
+                    $idosoba[]=$wiersz['id'];
+                }
+                if($_SERVER["REQUEST_METHOD"]=="POST"){
+                    $przedmiotID=$_POST["przedmiot"];
+                    $osoba=$_POST["osoba"];
+                    $ocena=$_POST["ocena"];
+                    $insert="INSERT INTO oceny VALUES(null,'$osoba','$przedmiotID','$ocena')";
+                    if(mysqli_query($connect,$insert)){
+                        echo "Dodano dane do bazy";
+                    }else{
+                        echo "Bład dodawania do bazy";
+                    }
+                }
+            }else{
+                echo "Brak połączenia z bazą";
             }
+            mysqli_close($connect);
         ?>
-    </select>
+    <form method="POST">
+        <label for="przedmiot">Wybierz przedmiot</label>
+        <select name="przedmiot" id="przedmiot">
+            <?php
+                for($i=0;$i<count($przedmiot);$i++){
+                    echo "<option value=$idprzedmiot[$i]> $przedmiot[$i]</option>";
+                }
+            ?>
 
-    <?php 
-        $polaczenie=mysqli_connect('localhost','root','','4ti');
-        if($polaczenie){
-            echo "Połączyliśmy się z bazą";
-            $zapytanie="SELECT * FROM dane";
-            $wynik=mysqli_query($polaczenie,$zapytanie);
-            while($wiersz=mysqli_fetch_array($wynik)){
-                $imie[]=$wiersz['imie'];
-                $nazwisko[]=$wiersz['nazwisko'];
-                $iddane[]=$wiersz['id'];
+        </select>
+        <br>
+        <label for="osoba">Wybierz osobę (ucznia)</label>
+        <select name="osoba" id="osoba">
+            <?php
+                for($i=0;$i<count($imie);$i++){
+                    echo "<option value=$idosoba[$i]> $imie[$i] $nazwisko[$i]</option>";
+                }
+            ?>
 
-            }
-        }else{
-            echo "błąd połączenia z bazą";
-        }
-        mysqli_close($polaczenie);
-    ?>
-
-        <label for="dane">Wybierz dane</label>
-    <select name="dane" id="dane">
-        <?php
-            for($i=0;$i<count($imie);$i++){
-                echo "<option value=$iddane[$i],> $imie[$i],$nazwisko[$i]</option>";
-            }
-        ?>
-    </select>
-</main>
+        </select>
+        <br>
+        <label for="ocena">Podaj ocenę</label>
+        <input type="number" name="ocena" id="ocena">
+        <br>
+        <input type="submit" value="Zapisz do bazy">
+    </form>
+    </main>
     <footer>
-        Strone wykonał Aleksander
+        <p>Stronę wykonał Przemek</p>
     </footer>
+    
 </body>
 </html>
